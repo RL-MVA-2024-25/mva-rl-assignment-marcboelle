2d4569f1e1dccb98c16b631b6aab6ced499489b6
marcboelle <marc.boelle@polytechnique.edu>
2025-01-10 17:09:57 +0100

    DQN commit

--- src/train.py
+++ src/train.py
@@ -1,4 +1,10 @@
 from gymnasium.wrappers import TimeLimit
+import torch
+import torch.nn as nn
+import numpy as np
+from copy import deepcopy
+
+
 from env_hiv import HIVPatient
 
 env = TimeLimit(
@@ -10,6 +16,23 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
+
+class Network(torch.nn.Module):
+    def __init__(self, state_dim, n_actions, n_layers=4, hidden_dim=256):
+        super(Network, self).__init__()
+
+        self.first_layer = nn.Sequential(nn.Linear(state_dim, hidden_dim), nn.ReLU())
+        self.last_layer = nn.Linear(hidden_dim, n_actions)
+        self.mlp = nn.ModuleList([nn.Sequential(nn.Linear(hidden_dim, hidden_dim), nn.ReLU()) for _ in range(n_layers)])
+
+    def forward(self, x):
+        x = self.first_layer(x)
+        for layer in self.mlp:
+            x = layer(x)
+        x = self.last_layer(x)
+        return x
+    
+
 class ProjectAgent:
     def act(self, observation, use_random=False):
         return 0
@@ -19,3 +42,47 @@ class ProjectAgent:
 
     def load(self):
         pass
+
+
+dqn_config = {
+    "n_layers":4,
+    "hidden_dim":256
+}
+
+
+class DQN(ProjectAgent):
+    def __init__(self, config=dqn_config):
+        # Init config
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = Network(env.observation_space.shape[0], env.action_space.n, config["n_layers"], config["hidden_dim"]).to(self.device)
+
+        #Init means and stds
+        self.state_mean = torch.tensor([3.6e5, 7.75e3, 287, 33, 36.8e3, 55], dtype=torch.float32).to(self.device)
+        self.state_std = torch.tensor([1.28788e5, 1.4435e4, 345, 25, 70.083e3, 32], dtype=torch.float32).to(self.device)
+        
+        self.reward_mean = 4.5344e4
+        self.reward_std = 3.3407e4
+
+    def normalize_state(self, state):
+        out = (state - self.state_mean) / self.state_std
+        return out
+    
+    def normalize_reward(self, reward):
+        return (reward - self.reward_mean) / self.reward_std
+
+    def act(self, observation, use_random=False):
+        observation = torch.tensor(observation, dtype=torch.float32).to(self.device)
+        observation = self.normalize_state(observation, self.state_mean, self.state_std).unsqueeze(0)
+        with torch.no_grad():
+            Q = self.model(observation)
+            action = torch.argmax(Q).item()
+        return action
+
+    def save(self, path):
+        torch.save(self.model.state_dict(), path)
+
+    def load(self):
+        self.model.load_state_dict(
+            torch.load("./models/best_model.pt", map_location=torch.device("cpu"))
+        )
+        self.model.eval()
